b0c3449b9573369aebfbe54a6d9fe69b4c14898c
andiaz <andiaz@aidacanarias.com>
2021-10-07 16:39:40 +0200

    add test: DuplicateCleannerShould

---
+++ DuplicateFilesKata.Tests/DuplicateCleannerShould.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace DuplicateFilesKata.Tests
+{
+    public class DuplicateCleannerShould
+    {
+        private readonly DuplicateCleanner _duplicateCleanner;
+
+        public DuplicateCleannerShould() {
+            _duplicateCleanner = new DuplicateCleanner();
+        }
+
+        [Fact]
+        public void return_a_list_of_strings() {
+            var aGivenListOfString = new List<string> { "anyString", "anyDiferentString"};
+
+            var  cleaned = _duplicateCleanner.Clean(aGivenListOfString);
+
+            Assert.Equal(aGivenListOfString, cleaned);
+        }
+    }
+}
---
+++ DuplicateFilesKata/DuplicateCleanner.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DuplicateFilesKata {
+    public class DuplicateCleanner {
+        public List<string> Clean(List<string> aGivenListOfString) {
+            return aGivenListOfString.ToList();
+        }
+    }
+}
